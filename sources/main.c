5cf4678620263272b491ca05e3933962194de2fb
sierra182 <sierra-echo@msn.com>
2023-11-17 09:13:32 +0100

    backtrack term

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 13:45:00 by svidot            #+#    #+#             */
-/*   Updated: 2023/11/16 22:53:26 by marvin           ###   ########.fr       */
+/*   Updated: 2023/11/17 09:10:54 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -147,74 +147,144 @@ int	is_sort(t_list *lst, int a_size)
 	return (1);
 }
 
-int	rec(t_list **a_head, t_list **b_head, int depth, int depth_max, int a_size, int **op_arr)
+enum e_op
+{
+	NONE,
+	PA,
+	PB,
+	SA,
+	SB,
+	SS,
+	RA,
+	RB,
+	RR,
+	RRA,
+	RRB,
+	RRR
+};
+
+void	print_tab(int *arr)
+{
+	fflush(stdout);
+	printf("\033[2K\r");	   
+	while(*arr)
+	{	
+		if (*arr == PA)
+			ft_printf("pa ");
+		else if (*arr == PB)
+			ft_printf("pb ");
+		else if (*arr == SA)
+			ft_printf("sa ");
+		else if (*arr == SB)
+			ft_printf("sb ");
+		else if (*arr == SS)
+			ft_printf("ss ");
+		else if (*arr == RA)
+			ft_printf("ra ");
+		else if (*arr == RB)
+			ft_printf("rb ");		
+		else if (*arr == RR)
+			ft_printf("rr ");
+		else if (*arr == RRA)
+			ft_printf("rra ");
+		else if (*arr == RRB)
+			ft_printf("rrb ");
+		else if (*arr == RRR)
+			ft_printf("rrr ");
+		arr++;
+	}
+}
+
+int	rec(t_list **a_head, t_list **b_head, int depth, int depth_max, int a_size, int *op_arr)
 {
 	if (is_sort(*a_head, a_size))
 	{
 		ft_printf("depth: %d\n", depth);
 		return (1);		
 	}	
-	else if (depth >= depth_max)	
+	else if (depth >= depth_max)
+	{
+		print_tab(op_arr);		
 		return (0);	
+	}
 	depth++;
 		
+	op_arr[depth - 1] = SA;
 	sa(*a_head);
-	if (rec(a_head, b_head, depth, depth_max, a_size))
-	{
-		op_arr[depth - 1] = 
+	if (rec(a_head, b_head, depth, depth_max, a_size, op_arr))		
 		return (1);
-	}
 	sa(*a_head);
+	op_arr[depth - 1] = 0;
 	
+	op_arr[depth - 1] = PB;
 	pb(a_head, b_head);
-	if (rec(a_head, b_head, depth, depth_max, a_size))
+	if (rec(a_head, b_head, depth, depth_max, a_size, op_arr))
 		return (1);
 	pa(a_head, b_head);
+	op_arr[depth - 1] = 0;
 	
+	op_arr[depth - 1] = SB;
 	sb(*b_head);
-	if (rec(a_head, b_head, depth, depth_max, a_size))
+	if (rec(a_head, b_head, depth, depth_max, a_size, op_arr))
 		return (1);
 	sb(*b_head);
+	op_arr[depth - 1] = 0;
 	
+	op_arr[depth - 1] = RA;
 	ra(a_head);
-	if (rec(a_head, b_head, depth, depth_max, a_size))
+	if (rec(a_head, b_head, depth, depth_max, a_size, op_arr))
 		return (1);
 	rra(a_head);
+	op_arr[depth - 1] = 0;
 	
+	op_arr[depth - 1] = RB;
 	rb(b_head);
-	if (rec(a_head, b_head, depth, depth_max, a_size))
+	if (rec(a_head, b_head, depth, depth_max, a_size, op_arr))
 		return (1);
 	rrb(b_head);
+	op_arr[depth - 1] = 0;
 	
+	op_arr[depth - 1] = RRA;
 	rra(a_head);
-	if (rec(a_head, b_head, depth, depth_max, a_size))
+	if (rec(a_head, b_head, depth, depth_max, a_size, op_arr))
 		return (1);
 	ra(a_head);
+	op_arr[depth - 1] = 0;
 	
+	op_arr[depth - 1] = RRB;
 	rrb(b_head);
-	if (rec(a_head, b_head, depth, depth_max, a_size))
+	if (rec(a_head, b_head, depth, depth_max, a_size, op_arr))
 		return (1);
 	rb(b_head);
+	op_arr[depth - 1] = 0;
 	
+	op_arr[depth - 1] = PA;
 	pa(a_head, b_head);
-	if (rec(a_head, b_head, depth, depth_max, a_size))
+	if (rec(a_head, b_head, depth, depth_max, a_size, op_arr))
 		return (1);
 	pb(a_head, b_head);
+	op_arr[depth - 1] = 0;
 	
+	op_arr[depth - 1] = SS;
 	ss(*a_head, *b_head);
-	if (rec(a_head, b_head, depth, depth_max, a_size))
+	if (rec(a_head, b_head, depth, depth_max, a_size, op_arr))
 		return (1);
 	ss(*a_head, *b_head);
+	op_arr[depth - 1] = 0;
 	
+	op_arr[depth - 1] = RR;
 	rr(a_head, b_head);
-	if (rec(a_head, b_head, depth, depth_max, a_size))
+	if (rec(a_head, b_head, depth, depth_max, a_size, op_arr))
 		return (1);
 	rrr(a_head, b_head);
+	op_arr[depth - 1] = 0;
 	
+	op_arr[depth - 1] = RRR;
 	rrr(a_head, b_head);
-	if (rec(a_head, b_head, depth, depth_max, a_size))
+	if (rec(a_head, b_head, depth, depth_max, a_size, op_arr))
 		return (1);
 	rr(a_head, b_head);
+	op_arr[depth - 1] = 0;
 	return (0);
 }
 
@@ -240,12 +310,14 @@ int	main(int argc, char *argv[])
 	//	if (is_ascending(a_head))
 	int	lstsize = ft_lstsize(a_head);
 	int	depth_max = 0;
-	while (!rec(&a_head, &b_head, 0, depth_max, lstsize))
-	{
-		ft_printf("\nKO\n");
-		depth_max++;
-	}			
+	int op_arr[11];
+	int	i = -1;
+	while (++i < 11)
+		op_arr[i] = 0;
+	while (!rec(&a_head, &b_head, 0, depth_max, lstsize, op_arr))	
+		depth_max++;			
 	print_lst(a_head, b_head);
+	//	print_tab(op_arr);
 	//print_lst(a_head, b_head);
 	// sa(a_head);
 	// print_lst(a_head, b_head);
