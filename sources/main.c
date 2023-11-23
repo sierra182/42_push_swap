f80f95b2c1fefc3c23270ea68fc9c310baa51d45
Sebastien Vidot <svidot@c3r1p2.42perpignan.fr>
2023-11-23 10:04:27 +0100

    starting alg

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: svidot <svidot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 13:45:00 by svidot            #+#    #+#             */
-/*   Updated: 2023/11/23 07:42:03 by svidot           ###   ########.fr       */
+/*   Updated: 2023/11/23 10:03:55 by svidot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,69 @@
 #include "operations.h"
 #include <stdio.h>
 
-void	
+int	get_max_value(t_list *lst)
+{
+	int	max_value;
+	
+	max_value = *(int *) lst->content;
+	while (lst)
+	{
+		if (*(int *) lst->content > max_value)
+			max_value = *(int *) lst->content;
+		lst = lst->next;
+	}
+	return (max_value);
+}
+
+int	get_min_value(t_list *lst)
+{
+	int	min_value;
+	
+	min_value = *(int *) lst->content;
+	while (lst)
+	{
+		if (*(int *) lst->content < min_value)
+			min_value = *(int *) lst->content;
+		lst = lst->next;
+	}
+	return (min_value);
+}
+
+int	calculate_maxrange(t_list *lst)
+{
+	int	range;
+	int	min_value;
+	int	max_value;
+	
+	range = 1;
+	min_value = get_min_value(lst);
+	max_value = get_max_value(lst);
+	while (min_value != max_value--)
+		range++;
+	return (range);
+}
+
+int	is_in_actualpiece(int actualpiece, int nbr, int range)
+{	
+	return (nbr >= (actualpiece - 1) * range && nbr <= (actualpiece) * range);	
+}
+
+void	alg(t_list *la, t_list *lb)
+{
+	int 	actual_piece;
+	t_list	*lstlast;
+	
+	lstlast = ft_lstlast(la);
+	actual_piece = 0;
+	while (++actual_piece <= 5)
+	{
+		while (la)
+		{
+			
+			la = la->next;
+		}
+	}
+}
 
 void	alg_forwarding(t_list **la, t_list **lb, int argc)
 {
@@ -44,6 +106,10 @@ int	main(int argc, char *argv[])
 	if (!setup(&argc, argv, argv, &args_arr))
 		return (write(2, "Error\n", 6));
 	init_list(&a_head, argc, args_arr);
+	
+	//ft_printf("%d\n", calculate_maxrange(a_head) / 5);	
+	ft_printf("%d\n", is_in_actualpiece(4, 580, calculate_maxrange(a_head) / 5));
+	
 	//int	lstsize = ft_lstsize(a_head);	
 	//ft_printf(" len: %d\n", lstsize); 	
 	//print_lst(a_head, b_head);	
