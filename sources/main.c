81bf76c0050b470ab6863487ec20d0145345efb5
Sebastien Vidot <svidot@c2r4p2.42perpignan.fr>
2023-11-24 10:07:53 +0100

    wip set sec

--- sources/main.c
+++ sources/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: svidot <svidot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 13:45:00 by svidot            #+#    #+#             */
-/*   Updated: 2023/11/23 21:01:29 by marvin           ###   ########.fr       */
+/*   Updated: 2023/11/24 10:05:32 by svidot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,32 +21,32 @@
 #include "operations.h"
 #include <stdio.h>
 
-int	get_max_value(t_list *lst)
+t_list	*get_max_item(t_list *lst)
 {
-	int	max_value;
+	t_list	*max_item;
 	
-	max_value = *(int *) lst->content;
+	max_item = lst;
 	while (lst)
 	{
-		if (*(int *) lst->content > max_value)
-			max_value = *(int *) lst->content;
+		if (*(int *) lst->content > *(int *) max_item->content)
+			max_item = lst;
 		lst = lst->next;
 	}
-	return (max_value);
+	return (max_item);
 }
 
-int	get_min_value(t_list *lst)
+t_list	*get_min_item(t_list *lst)
 {
-	int	min_value;
+	t_list	*min_item;
 	
-	min_value = *(int *) lst->content;
+	min_item = lst;
 	while (lst)
 	{
-		if (*(int *) lst->content < min_value)
-			min_value = *(int *) lst->content;
+		if (*(int *) lst->content < *(int *) min_item->content)
+			min_item = lst;
 		lst = lst->next;
 	}
-	return (min_value);
+	return (min_item);
 }
 #include <stdio.h>
 #define GROUP 5
@@ -58,8 +58,8 @@ double	get_range(t_list *lst)
 	int		max_value;
 	
 	range = 1;
-	min_value = get_min_value(lst);
-	max_value = get_max_value(lst);
+	min_value = *(int *) get_min_item(lst)->content;
+	max_value = *(int *) get_max_item(lst)->content;
 	while (min_value != max_value--)
 		range++;
 	//printf("range: %f\n", range);
@@ -76,7 +76,7 @@ t_list	*get_top_item(t_list *lst, double range, int actual_piece)
 {	
 	int	min_value;
 	
-	min_value = get_min_value(lst);
+	min_value = *(int *) get_min_item(lst)->content;
 	while (lst)
 	{
 		if (is_in_actualpiece(min_value, actual_piece, *(int *) lst->content, range))
@@ -92,7 +92,7 @@ t_list	*get_bot_item(t_list *lst, double range, int actual_piece)
 	t_list	*bot_item;
 	
 	bot_item = NULL; 
-	min_value = get_min_value(lst);
+	min_value = *(int *) get_min_item(lst)->content;
 	while (lst)
 	{
 		if (is_in_actualpiece(min_value, actual_piece, *(int *) lst->content, range))
@@ -146,13 +146,35 @@ t_list	*choose_one(t_list **la, t_list **lb, int lstsize, t_list *top_item, t_li
 	}
 	return (one);
 }
-void	set_second_list(t_list *one, t_list **lst)
+
+void	set_secondlist(t_list **la, t_list **lb, t_list *one, int lstsize)
 {
-	while (*lst)
+	t_list	*to_up;
+	t_list	*tmp;
+	int		to_up_ind;
+	
+	tmp = *lb;
+	to_up =  get_min_item(*lb);
+	while (tmp)
 	{
-		if (*(int *) (*lst)->content > *(int *) one->content)
-			
-		*lst = (*lst)->next;
+		if (*(int *) tmp->content > *(int *) to_up->content && *(int *) tmp->content < *(int *) one->content)
+			to_up = tmp;
+		tmp = tmp->next;
+	}
+	to_up_ind = get_item_index(*lb, to_up);
+	ft_printf("WAAA TOUP%d\n", to_up_ind);
+	if (to_up_ind > 0 && lstsize - to_up_ind >= to_up_ind)
+	{	ft_printf("WAAA%d\n", to_up_ind);
+		while (to_up_ind-- > 0)
+		{
+			ft_printf("%d", to_up_ind);
+			rb(la, lb, 1);		
+		}
+	}
+	else if (to_up_ind > 0)
+	{		
+		while (lstsize-- - to_up_ind)
+			rrb(la, lb, 1);	
 	}
 }
 void	alg(t_list **la, t_list **lb)
@@ -182,7 +204,7 @@ void	alg(t_list **la, t_list **lb)
 			one = choose_one(la, lb, ft_lstsize(*la), top_item, bot_item);
 			if (one)
 			{
-				set_secondlist(one, lb);
+				set_secondlist(la, lb, one, ft_lstsize(*lb));
 				pb(la, lb, 1);
 				ft_printf("%d\n", *(int *) one->content);
 			}
