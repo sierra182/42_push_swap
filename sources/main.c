c6b92b5662d74fd56128490b9746329db74d10e9
Sebastien Vidot <svidot@c3r1p2.42perpignan.fr>
2023-11-25 11:48:10 +0100

    add optim easy

--- sources/main.c
+++ sources/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: svidot <svidot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 13:45:00 by svidot            #+#    #+#             */
-/*   Updated: 2023/11/24 21:27:48 by marvin           ###   ########.fr       */
+/*   Updated: 2023/11/25 11:42:45 by svidot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ t_list	*get_min_item(t_list *lst)
 	return (min_item);
 }
 #include <stdio.h>
-#define GROUP 11
+#define GROUP 5
 
 double	get_range(t_list *lst)
 {
@@ -112,7 +112,7 @@ int	get_item_index(t_list *lst, t_list *item)
 	return (--i);
 }
 
-t_list	*choose_one(t_list **la, t_list **lb, int lstsize, t_list *top_item, t_list *bot_item)
+t_list	*choose_one(t_list **la, t_list **lb, int lstsize, t_list *top_item, t_list *bot_item, t_eop **sol_arr)
 {
 	t_list	*one;
 	int		top_ind;
@@ -126,28 +126,35 @@ t_list	*choose_one(t_list **la, t_list **lb, int lstsize, t_list *top_item, t_li
 	if (top_item)
 		top_ind = get_item_index(*la, top_item);
 	if (bot_item) 
-		bot_ind = get_item_index(*la, bot_item);	
+		bot_ind = get_item_index(*la, bot_item);				
 	if (lstsize - bot_ind >= top_ind)
 	{
 		one = top_item;
 		while (top_ind--)
-			ra(la, lb, 1);		
+		{			
+			ra(la, lb, 0);		
+			*(*sol_arr)++ = RA;
+		}		
 	}
 	else if (lstsize - bot_ind < top_ind)
-	{
+	{	
 		one = bot_item;
-		while (lstsize-- - bot_ind)
-			rra(la, lb, 1);		
+		while (lstsize-- - bot_ind)	
+		{			
+			rra(la, lb, 0);		
+			*(*sol_arr)++ = RRA;
+		}		
 	}
+		
 	return (one);
 }
 
-void	set_secondlist(t_list **la, t_list **lb, t_list *one, int lstsize)
+void	set_secondlist(t_list **la, t_list **lb, t_list *one, int lstsize, t_eop **sol_arr)
 {
 	t_list	*to_up;
 	t_list	*tmp;
 	int		to_up_ind;
-	
+
 	tmp = *lb;
 	to_up =  get_min_item(*lb);
 	if (to_up && *(int *) to_up->content > *( int *) one->content)	
@@ -160,39 +167,102 @@ void	set_secondlist(t_list **la, t_list **lb, t_list *one, int lstsize)
 				to_up = tmp;
 			tmp = tmp->next;
 		}
-	}
+	}	
 	to_up_ind = get_item_index(*lb, to_up);
 	if (to_up_ind > 0 && lstsize - to_up_ind >= to_up_ind)
 	{	
 		while (to_up_ind-- > 0)
-			rb(la, lb, 1);				
+		{			
+			rb(la, lb, 0);				
+			*(*sol_arr)++ = RB;
+		}
 	}
 	else if (to_up_ind > 0)
 	{		
 		while (lstsize-- - to_up_ind)
-			rrb(la, lb, 1);	
+		{
+			rrb(la, lb, 0);				
+			*(*sol_arr)++ = RRB;
+		}
 	}
 }
 
-void	rewind_secondlist(t_list **la, t_list **lb, int lstsize)
+void	rewind_secondlist(t_list **la, t_list **lb, int lstsize, t_eop *sol_arr[])
 {	
 	t_list	*to_up;
 	int		to_up_ind;
-	
+
 	to_up =  get_max_item(*lb);	
 	to_up_ind = get_item_index(*lb, to_up);
 	if (to_up_ind > 0 && lstsize - to_up_ind >= to_up_ind) 
 	{	
 		while (to_up_ind-- > 0)
-			rb(la, lb, 1);			
+		{
+			rb(la, lb, 0);				
+			*(*sol_arr)++ = RB;
+		}
 	}
 	else if (to_up_ind > 0)
 	{		
 		while (lstsize-- - to_up_ind)
-			rrb(la, lb, 1);	
+		{		
+			rrb(la, lb, 0);	
+			*(*sol_arr)++ = RRB;
+		}
 	}
 }
+void	delone_sol(t_eop **sol_arr)
+{//ft_printf("del deb\n");
+	t_eop	*sol_sav;
+	
+	sol_sav = *sol_arr;
+	while (*(*sol_arr + 1))
+	{//ft_printf("del deb\n");
+		**sol_arr = *(*sol_arr + 1); 
+		(*sol_arr)++;
+	}
+	**sol_arr = 0;
+	*sol_arr = sol_sav;
+	//ft_printf("del fin\n");
+}
 
+void	sol_optim(t_eop **sol_arr)
+{
+	int		n_ra;
+	int		n_rb;
+	
+	n_ra = 0;
+	n_rb = 0;
+	while (**sol_arr)
+	{	
+		n_ra = 0;	
+		while (**sol_arr == RA)
+		{		
+			n_ra++;			
+			(*sol_arr)++;
+		}
+		n_rb = 0;
+		while ((**sol_arr == RB && n_ra--))
+		{		
+			*(*sol_arr - (++n_rb)) = RR;				
+			delone_sol(sol_arr);			
+		}
+		n_ra = 0;
+		while (**sol_arr == RRA)
+		{		
+			n_ra++;			
+			(*sol_arr)++;
+		}
+		n_rb = 0;
+		while ((**sol_arr == RRB && n_ra--))
+		{		
+			*(*sol_arr - (++n_rb)) = RRR;				
+			delone_sol(sol_arr);			
+		}				
+		(*sol_arr)++;
+	}
+	
+}
 void	alg(t_list **la, t_list **lb)
 {
 	t_list	*top_item;
@@ -201,8 +271,15 @@ void	alg(t_list **la, t_list **lb)
 	int 	actual_piece;
 	double	range;
 	
+	t_eop sl_arr[20000];
+	
+	int	i = -1;
+	while (++i < 20000)
+		sl_arr[i] = 0;
+	t_eop *sol_arr = sl_arr;		
 	range = get_range(*la);	
 	actual_piece = 0;
+	
 	while (++actual_piece <= 5)
 	{
 		while (*la)
@@ -211,17 +288,30 @@ void	alg(t_list **la, t_list **lb)
 			if (!top_item)		
 				break;
 			bot_item = get_bot_item(*la, range, actual_piece);		
-			one = choose_one(la, lb, ft_lstsize(*la), top_item, bot_item);
+			one = choose_one(la, lb, ft_lstsize(*la), top_item, bot_item, &sol_arr);
 			if (one)
 			{		
-				set_secondlist(la, lb, one, ft_lstsize(*lb));		
-				pb(la, lb, 1);				
+				set_secondlist(la, lb, one, ft_lstsize(*lb), &sol_arr);		
+				pb(la, lb, 0);	
+				*sol_arr++ = PB;					
 			}					
+			
 		}	
 	}
-	rewind_secondlist(la, lb, ft_lstsize(*lb));		
-	while (pa(la, lb, 1))
-	 	;
+	rewind_secondlist(la, lb, ft_lstsize(*lb), &sol_arr);
+
+	while (pa(la, lb, 0))
+		*sol_arr++ = PA;
+	
+	sol_arr = sl_arr;
+	char	*op_char_arr[OP];
+	init_op_char_arr(op_char_arr);
+	//print_tab(sol_arr, op_char_arr);
+	sol_optim(&sol_arr);
+//	ft_printf("choose another alg\n");
+	sol_arr = sl_arr;
+	print_tab(sol_arr, op_char_arr);
+	//print_lst(*la, *lb);
 }
 
 void	alg_forwarding(t_list **la, t_list **lb, int argc)
